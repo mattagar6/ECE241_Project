418c1d25e1dcb9b2a1e2779a0f5cba884dd474a1
Matthew Agar <mattagar6@gmail.com>
2019-11-24 20:31:07 -0500

    Update TopControl.v
    added basic scoring functionality

--- TopControl.v
+++ TopControl.v
@@ -3,6 +3,7 @@ module topControl(
 		input [3:0] KEY, 
 		input [9:0] SW,
 		output [9:0] LEDR, 
+		output [6:0] HEX0,
 		output VGA_CLK, 
 		output VGA_HS,
 		output VGA_VS, 
@@ -20,6 +21,8 @@ module topControl(
 	reg [7:0] yoffsetset;
 	reg [23:0] delayCnt;
 	reg [23:0] curDelay;
+	reg [3:0] score;
+	reg hit;
 	
 	initial begin
 		xoffsetset <= 9'd160;
@@ -44,6 +47,7 @@ module topControl(
 			delayCnt <= delayCnt - 1;
 		if(curDelay == 0) 
 			curDelay <= DELAY;
+		score <= score + hit; // increment score on a hit
 	end
 	
 	Graphics u0(
@@ -66,8 +70,38 @@ module topControl(
 					.reset_b(~SW[0]), // dummy switch
 					.go(~KEY[3]),
 					.stream(xoffsetset),
-					.hit(LEDR[0])
+					.hit(hit)
 					);
-		
-		
+	
+	// temporary, will hook up to VGA later
+	hex_decoder u2(.hex_digit(score), 
+		       .segments(HEX0)
+		      );
+	
+endmodule
+
+module hex_decoder(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
 endmodule
