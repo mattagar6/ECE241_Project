78741c5b5c502a6ac5df4362e6b293f96337afd8
Matthew Agar <mattagar6@gmail.com>
2019-11-25 16:43:48 -0500

    Update TopControl.v
    changed "reg hit" => "wire hit"

--- TopControl.v
+++ TopControl.v
@@ -22,7 +22,7 @@ module topControl(
 	reg [23:0] delayCnt;
 	reg [23:0] curDelay;
 	reg [3:0] score;
-	reg hit;
+	wire hit;
 	
 	initial begin
 		xoffsetset <= 9'd160;
@@ -47,8 +47,10 @@ module topControl(
 			delayCnt <= delayCnt - 1;
 		if(curDelay == 0) 
 			curDelay <= DELAY;
-		score <= score + hit; // increment score on a hit
 	end
+	
+	always@(posedge hit)
+		score <= score + 1; // increment score on a hit
 	
 	Graphics u0(
 					.CLOCK_50(CLOCK_50),
@@ -78,30 +80,4 @@ module topControl(
 		       .segments(HEX0)
 		      );
 	
-endmodule
-
-module hex_decoder(hex_digit, segments);
-    input [3:0] hex_digit;
-    output reg [6:0] segments;
-   
-    always @(*)
-        case (hex_digit)
-            4'h0: segments = 7'b100_0000;
-            4'h1: segments = 7'b111_1001;
-            4'h2: segments = 7'b010_0100;
-            4'h3: segments = 7'b011_0000;
-            4'h4: segments = 7'b001_1001;
-            4'h5: segments = 7'b001_0010;
-            4'h6: segments = 7'b000_0010;
-            4'h7: segments = 7'b111_1000;
-            4'h8: segments = 7'b000_0000;
-            4'h9: segments = 7'b001_1000;
-            4'hA: segments = 7'b000_1000;
-            4'hB: segments = 7'b000_0011;
-            4'hC: segments = 7'b100_0110;
-            4'hD: segments = 7'b010_0001;
-            4'hE: segments = 7'b000_0110;
-            4'hF: segments = 7'b000_1110;   
-            default: segments = 7'h7f;
-        endcase
 endmodule
